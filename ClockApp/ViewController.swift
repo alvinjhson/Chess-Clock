80b891501b0d2e375e1b5e2960d90fd6bf4ad0c0
alvinjhson <alvinbest26@gmail.com>
2024-03-11 12:11:08 +0100

    Initial Commit

---
+++ ClockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClockApp
+//
+//  Created by Alvin Johansson on 2024-03-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
